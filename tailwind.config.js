8957bc4176bdd3e976148e4e0fdcc7582b97efc9
Mehedi Hasan <48385428+mehedi6463@users.noreply.github.com>
2024-05-08 22:41:28 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary": "#003C43",
+        "secondery": "#77B0AA",
+        "thirdColor": "#E3FEF7",
+        "fourthColor": "#7ABA78"
+      }
+    },
+  },
+  plugins: [],
+}
